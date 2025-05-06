02b53f8745abdcb15547afa36c7519acf56574e5
igorpaes21 <irsp2121@hmail.com>
2025-05-05 22:23:08 -0300

    first commit

---
+++ src/main/java/br/com/senai/api/reciclaville/ReciclaVilleApplication.java
@@ -0,0 +1,13 @@
+package br.com.senai.api.reciclaville;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReciclaVilleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReciclaVilleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/senai/api/reciclaville/ReciclaVilleApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.senai.api.reciclaville;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReciclaVilleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
