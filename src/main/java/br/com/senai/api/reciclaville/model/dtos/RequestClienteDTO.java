c8f6a06dc78f9ecfbb04d538596c399aa77e87d8
igorpaes21 <irsp2121@hmail.com>
2025-05-07 20:04:27 -0300

    Criação das classes para requisições e resposta da entidade cliente.

--- src/main/java/br/com/senai/api/reciclaville/model/dtos/RequestClienteDTO.java
+++ src/main/java/br/com/senai/api/reciclaville/model/dtos/RequestClienteDTO.java
@@ -1,4 +1,24 @@
 package br.com.senai.api.reciclaville.model.dtos;
 
+import jakarta.validation.constraints.NotBlank;
+import lombok.Data;
+
+@Data
 public class RequestClienteDTO {
+
+    @NotBlank(message = "O nome não pode ser vazio")
+    private String nome;
+    @NotBlank(message = "O CNPJ não pode ser vazio")
+    private String cnpj;
+    @NotBlank(message = "A atividade econômica não pode ser vazia")
+    private String atividadeEconomica;
+    @NotBlank(message = "O responsável não pode ser vazio")
+    private String responsavel;
+
+    public RequestClienteDTO(String nome, String cnpj, String atividadeEconomica, String responsavel) {
+        this.nome = nome;
+        this.cnpj = cnpj;
+        this.atividadeEconomica = atividadeEconomica;
+        this.responsavel = responsavel;
+    }
 }
--- src/main/java/br/com/senai/api/reciclaville/model/dtos/ResponseClienteDTO.java
+++ src/main/java/br/com/senai/api/reciclaville/model/dtos/ResponseClienteDTO.java
@@ -1,4 +1,21 @@
 package br.com.senai.api.reciclaville.model.dtos;
 
+import lombok.Data;
+
+@Data
 public class ResponseClienteDTO {
+
+    private Long id;
+    private String nome;
+    private String cnpj;
+    private String atividadeEconomica;
+    private String responsavel;
+
+    public ResponseClienteDTO(Long id, String nome, String cnpj, String atividadeEconomica, String responsavel) {
+        this.id = id;
+        this.nome = nome;
+        this.cnpj = cnpj;
+        this.atividadeEconomica = atividadeEconomica;
+        this.responsavel = responsavel;
+    }
 }
