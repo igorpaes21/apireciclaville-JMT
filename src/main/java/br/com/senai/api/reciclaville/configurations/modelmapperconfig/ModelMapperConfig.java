8e09001dc6bcd75163d585202a26281c2dbff232
igorpaes21 <irsp2121@hmail.com>
2025-05-06 22:21:14 -0300

    Criação da classe ModelMapper e injeção da dependência da mesma.

---
+++ src/main/java/br/com/senai/api/reciclaville/configurations/modelmapperconfig/ModelMapperConfig.java
@@ -0,0 +1,14 @@
+package br.com.senai.api.reciclaville.configurations.modelmapperconfig;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ModelMapperConfig {
+
+    @Bean
+    public  ModelMapper modelMapper() {
+        return new ModelMapper();
+    }
+}
