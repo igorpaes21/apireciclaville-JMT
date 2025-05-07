29cc81c3e9cb9996ba7aab39d284208159561992
igorpaes21 <irsp2121@hmail.com>
2025-05-06 22:23:44 -0300

    Immplementação da classe de controle da entidade Cliente com toda a lógica para buscar todos os clientes, buscar cliente por id, criar, atualizar e deletar um cliente.

---
+++ src/main/java/br/com/senai/api/reciclaville/controller/ClienteController.java
@@ -0,0 +1,69 @@
+package br.com.senai.api.reciclaville.controller;
+
+import br.com.senai.api.reciclaville.model.dtos.RequestClienteDTO;
+import br.com.senai.api.reciclaville.model.dtos.ResponseClienteDTO;
+import br.com.senai.api.reciclaville.model.entity.Cliente;
+import br.com.senai.api.reciclaville.service.ClienteService;
+import jakarta.validation.Valid;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@RestController
+@RequestMapping("/clientes")
+public class ClienteController {
+
+    @Autowired
+    ClienteService clienteService;
+
+    @Autowired
+    ModelMapper modelMapper;
+
+    //Método para listar todos os Clientes
+    @GetMapping
+    public ResponseEntity<List<ResponseClienteDTO>> list() {
+        List<ResponseClienteDTO> clientes = this.clienteService.findAllClientes().stream()
+                .map(cliente -> modelMapper.map(cliente, ResponseClienteDTO.class)).collect(Collectors.toList());
+        return clientes.isEmpty() ? ResponseEntity.noContent().build() : ResponseEntity.ok(clientes);
+    }
+
+    //Método para buscar um Cliente pelo Id
+    @GetMapping("/{id}")
+    public ResponseEntity<ResponseClienteDTO> findById(@PathVariable Long id) {
+        Cliente cliente = clienteService.findClienteById(id);
+        ResponseClienteDTO clienteDTO = modelMapper.map(cliente, ResponseClienteDTO.class);
+        return ResponseEntity.ok(clienteDTO);
+    }
+
+    //Método para criar um Cliente
+    @PostMapping
+    public ResponseEntity<ResponseClienteDTO> create(@RequestBody @Valid RequestClienteDTO clienteDTO) throws Exception {
+        Cliente cliente = modelMapper.map(clienteDTO, Cliente.class);
+        Cliente createdCliente = clienteService.create(cliente);
+        ResponseClienteDTO createdClienteDTO = modelMapper.map(createdCliente, ResponseClienteDTO.class);
+
+        return ResponseEntity.status(HttpStatus.CREATED).body(createdClienteDTO);
+    }
+
+    //Método para atualizar um Cliente
+    @PutMapping("/{id}")
+    public ResponseEntity<ResponseClienteDTO> update(@PathVariable Long id, @RequestBody RequestClienteDTO clienteDTO) throws Exception {
+        Cliente cliente = modelMapper.map(clienteDTO, Cliente.class);
+        Cliente clienteUpdate = this.clienteService.update(id, cliente);
+        ResponseClienteDTO clienteUpdateDTO = modelMapper.map(clienteUpdate, ResponseClienteDTO.class);
+        return ResponseEntity.ok(clienteUpdateDTO);
+    }
+
+    //Método para deletar um Cliente
+    @DeleteMapping("/{id}")
+    public ResponseEntity<String> delete(@PathVariable Long id) {
+        this.clienteService.delete(id);
+        return ResponseEntity.ok("Cliente deletado com sucesso");
+
+    }
+}
