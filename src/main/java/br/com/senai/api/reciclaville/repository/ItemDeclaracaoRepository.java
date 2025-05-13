2a5257388566ef7ff7e408da53db16abaf6e2697
igorpaes21 <irsp2121@hmail.com>
2025-05-13 20:57:27 -0300

    Criação do repository extendendo a classe JpaRepository.

---
+++ src/main/java/br/com/senai/api/reciclaville/repository/ItemDeclaracaoRepository.java
@@ -0,0 +1,7 @@
+package br.com.senai.api.reciclaville.repository;
+
+import br.com.senai.api.reciclaville.model.entity.ItemDeclaracao;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ItemDeclaracaoRepository extends JpaRepository<ItemDeclaracao, Long> {
+}
