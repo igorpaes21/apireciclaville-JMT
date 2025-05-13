de5d6a42a58fd65c82cc5984ef0b63c2cc95128e
igorpaes21 <irsp2121@hmail.com>
2025-05-13 20:58:18 -0300

    Criação dos dtos da entidade "ItemDeclaracao" para requisições e respostas do sistema.

---
+++ src/main/java/br/com/senai/api/reciclaville/model/dtos/RequestItemDeclaracaoDTO.java
@@ -0,0 +1,10 @@
+package br.com.senai.api.reciclaville.model.dtos;
+
+import lombok.Data;
+
+@Data
+public class RequestItemDeclaracaoDTO {
+
+    private Long materialId;
+    private Double toneladasDeclaradas;
+}
---
+++ src/main/java/br/com/senai/api/reciclaville/model/dtos/ResponseItemDeclaracaoDTO.java
@@ -0,0 +1,15 @@
+package br.com.senai.api.reciclaville.model.dtos;
+
+import lombok.Data;
+
+@Data
+public class ResponseItemDeclaracaoDTO {
+
+    private Long id;
+    private Long materialId;
+    private String materialNome;
+    private Double toneladasDeclaradas;
+    private Double toneladasCompensacao;
+    private Double percentualCompensacao;
+
+}
