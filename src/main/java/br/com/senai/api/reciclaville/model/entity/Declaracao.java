1027a6a78f78b180a576623b24b3b4c878ccf53c
igorpaes21 <irsp2121@hmail.com>
2025-05-17 22:49:29 -0300

    Atualização das entidades e anotações.

--- src/main/java/br/com/senai/api/reciclaville/model/entity/Declaracao.java
+++ src/main/java/br/com/senai/api/reciclaville/model/entity/Declaracao.java
@@ -1,10 +1,12 @@
 package br.com.senai.api.reciclaville.model.entity;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.math.BigDecimal;
 import java.time.LocalDate;
 import java.util.List;
 
@@ -18,20 +20,30 @@ public class Declaracao {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    @ManyToOne
-    @JoinColumn(name = "fk_cliente_id", nullable = false)
-    private Cliente cliente;
-    @Column(nullable = false)
+
+    @Column(nullable = false, updatable = false)
     private LocalDate dataDeclaracao;
+
+    @ManyToOne
+    @JoinColumn(name = "cliente_id")
+    @NotNull(message = "O cliente é obrigatório.")
+    private Cliente cliente;
+
+    @NotNull(message = "A data inicial é obrigatório.")
     @Column(nullable = false)
     private LocalDate dataInicialPeriodo;
+
+    @NotNull(message = "A data final é obrigatório.")
     @Column(nullable = false)
     private LocalDate dataFinalPeriodo;
+
     @Column(nullable = false)
-    private Double totalMateriais;
+    private BigDecimal totalMateriais;
+
     @Column(nullable = false)
-    private Double totalCompensado;
-    @OneToMany(mappedBy = "declaracao", cascade = CascadeType.ALL)
+    private BigDecimal totalCompensado;
+
+    @OneToMany(mappedBy = "declaracao", cascade = CascadeType.ALL, orphanRemoval = true)
     private List<ItemDeclaracao> itens;
 
 }
