2011ae0c062e9b19bbda674585de751da20cf026
igorpaes21 <irsp2121@hmail.com>
2025-05-17 22:43:12 -0300

    Atualização das entidades e sua anotações.

--- src/main/java/br/com/senai/api/reciclaville/model/entity/Cliente.java
+++ src/main/java/br/com/senai/api/reciclaville/model/entity/Cliente.java
@@ -1,6 +1,7 @@
 package br.com.senai.api.reciclaville.model.entity;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -18,24 +19,21 @@ public class Cliente {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @NotNull(message = "O nome do cliente é obrigatório.")
     @Column(nullable = false)
     private String nome;
+
+    @NotNull(message = "O CNPJ do cliente é obrigatório.")
     @Column(nullable = false, unique = true)
     private String cnpj;
+
+    @NotNull(message = "A atividade econômica é obrigatório.")
     @Column(nullable = false)
     private String atividadeEconomica;
+
+    @NotNull(message = "O nome do responsável é obrigatório.")
     @Column(nullable = false)
     private String responsavel;
 
-//    public Collection<Object> getRoles() {
-//        return null;
-//    }
-//
-//    public String getPassword() {
-//        return null;
-//    }
-//
-//    public String getUsername() {
-//        return null;
-//    }
 }
