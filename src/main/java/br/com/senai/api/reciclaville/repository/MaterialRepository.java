0473faed4f5ab426e0d985f5965d8355111e7f24
igorpaes21 <irsp2121@hmail.com>
2025-05-17 22:56:34 -0300

    Implementação de metodo para persistir a procura do nome do material.

--- src/main/java/br/com/senai/api/reciclaville/repository/MaterialRepository.java
+++ src/main/java/br/com/senai/api/reciclaville/repository/MaterialRepository.java
@@ -1,7 +1,11 @@
 package br.com.senai.api.reciclaville.repository;
 
 import br.com.senai.api.reciclaville.model.entity.Material;
+import jakarta.validation.constraints.NotNull;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface MaterialRepository extends JpaRepository <Material, Long> {
+    Optional<Material> findByNomeMaterial(@NotNull(message = "O nome do material é obrigatório.") String nomeMaterial);
 }
