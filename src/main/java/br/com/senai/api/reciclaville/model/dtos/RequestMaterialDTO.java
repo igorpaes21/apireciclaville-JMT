50b56ab38204a75751c427667a79009a4222a102
igorpaes21 <irsp2121@hmail.com>
2025-05-17 23:00:28 -0300

    Implementação de anotações e atuaização dos nomes das entidades.

--- src/main/java/br/com/senai/api/reciclaville/model/dtos/RequestMaterialDTO.java
+++ src/main/java/br/com/senai/api/reciclaville/model/dtos/RequestMaterialDTO.java
@@ -1,12 +1,19 @@
 package br.com.senai.api.reciclaville.model.dtos;
 
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 
 @Data
 public class RequestMaterialDTO {
 
+    @NotNull
+    @Size(min = 3, max = 50, message = "O nome do material deve ter entre 3 e 50 caracteres.")
     private String nomeMaterial;
 
-    private Double percentualCompensacao;
+    @NotNull(message = "A percentagem de compensação é obrigatória.")
+    @Min(value = 1, message = "A percentagem de compensação deve ser maior ou igual a 0.")
+    private Double percentagemCompensacao;
 
 }
